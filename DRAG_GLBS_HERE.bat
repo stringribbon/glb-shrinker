791b80235a421b5f13e511d8e1ee4273552226df
Daniel Calleja <pigrsi.boat@gmail.com>
2021-12-06 18:54:57 +0000

    always use variable without quotes

--- DRAG_GLBS_HERE.bat
+++ DRAG_GLBS_HERE.bat
@@ -1,30 +1,32 @@
 @echo off
 set here=%~dp0
 
-if %1=="" goto end
+if "%~1"=="" goto end
 
 if not exist "%here%/compressed_output" mkdir "%here%/compressed_output"
 
 :processglb
-echo "About to unpack %1"
+echo "About to unpack %~1"
 if not exist "%here%/tmp" mkdir "%here%/tmp"
 
 :: empty tmp
 del /f /s /q "%here%/tmp" 1>nul
 
 :: unpack
-call gltf-pipeline -i %1 -o "%here%/tmp/tmp.gltf" --separate --json
+call gltf-pipeline -i "%~1" -o "%here%/tmp/tmp.gltf" --separate --json
 
 :: compress jpgs
 echo y | call npx imagemin-cli "%here%/tmp/*.jpg" --plugin.mozjpeg --out-dir="%here%/tmp"
+:: echo y | call npx imagemin-cli "%here%/tmp/*.png" --plugin.pngquant.quality={0.1,0.2} --out-dir="%here%/tmp"
 
 :: repack glb
 call gltf-pipeline -i "%here%/tmp/tmp.gltf" -o "%here%/compressed_output/%~n1.glb" --binary --draco --draco.compressMeshes
 
 :: move onto next item
+echo "%~1"
 echo:
 shift
-if %1=="" goto end
+if "%~1"=="" goto end
 goto processglb
 
 :end
@@ -32,8 +34,8 @@ echo "Processing complete! Compressed GLBs can be found in the compressed_output
 
 :: get rid of tmp
 if not exist "%here%/tmp" goto goodbye
-::del /f /s /q "%here%/tmp" 1>nul
-::rmdir /s /q "%here%/tmp"
+del /f /s /q "%here%/tmp" 1>nul
+rmdir /s /q "%here%/tmp"
 
 :goodbye
 pause
--- setup/RUN_THIS_FIRST.bat
+++ setup/RUN_THIS_FIRST.bat
@@ -1,6 +1,7 @@
 call npm install -g gltf-pipeline
 call npm install -g imagemin-cli
 call npm install -g imagemin-mozjpeg
+:: call npm install -g imagemin-pngquant
 echo y | npx imagemin-cli
 
 pause
